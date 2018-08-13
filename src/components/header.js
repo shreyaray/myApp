f27e4e89d4af46b63c062b329f29399576323914
Ray <rays02@thryv.com>
2018-08-13 05:27:32 -0500

    header stateless component

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,28 @@
 import React from 'react';
 import { Navbar, Nav, NavItem} from 'react-bootstrap';
 
-export class Header extends React.Component{
-    render(){
-        return(
-            <Navbar inverse className="navStyle">
-                <Navbar.Header>
-                    {/* props.children render any html which has passed externally from parent component */}
-                    <Navbar.Brand>
-                        {this.props.children} 
-                    </Navbar.Brand>
-                </Navbar.Header>
-                <Nav>
-                    <NavItem eventKey={1} href="#">
-                        Home
-                    </NavItem>
-                    <NavItem eventKey={2} href="#">
-                        Category
-                    </NavItem>
-                </Nav>
-                <Nav pullRight>
-                    <NavItem eventKey={1} href="#">
-                        Login
-                    </NavItem>
-                </Nav>
-            </Navbar>
-        );
-    }
+export const Header = (props) =>{
+    return(
+        <Navbar inverse className="navStyle">
+            <Navbar.Header>
+                {/* props.children render any html which has passed externally from parent component */}
+                <Navbar.Brand>
+                    {props.children} 
+                </Navbar.Brand>
+            </Navbar.Header>
+            <Nav>
+                <NavItem eventKey={1} href="#">
+                    Home
+                </NavItem>
+                <NavItem eventKey={2} href="#">
+                    Category
+                </NavItem>
+            </Nav>
+            <Nav pullRight>
+                <NavItem eventKey={1} href="#">
+                    Login
+                </NavItem>
+            </Nav>
+        </Navbar>
+    );
 }
